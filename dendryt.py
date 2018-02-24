a36f00938fa188919c95e58e2fc5c7f221edbf38
Nicolas CARPi <nicolas.carpi@curie.fr>
2018-02-25 00:41:11 +0100

    random is working

--- dendryt.py
+++ dendryt.py
@@ -33,66 +33,91 @@ class Dendryt():
     def moveCell(self):
 
         direction = self.getDirection()
-        print("cell pos: ", self.cellPos)
-        print("Direction:", direction)
+        #print("cell pos: ", self.cellPos)
+        #print("Direction:", direction)
 
         try:
             if direction == 'N':
                 newPos = self.cellPos[0] - 1
-                newCoord = [self.cellPos[0] - 1, self.cellPos[1]]
+                if newPos < 0:
+                    newPos = 0
+                newCoord = [newPos, self.cellPos[1]]
             elif direction == 'E':
                 newPos = self.cellPos[1] + 1
-                newCoord = [self.cellPos[0], self.cellPos[1] + 1]
+                if newPos < 0:
+                    newPos = 0
+                newCoord = [self.cellPos[0], newPos]
             elif direction == 'S':
                 newPos = self.cellPos[0] + 1
-                newCoord = [self.cellPos[0] + 1, self.cellPos[1]]
+                if newPos < 0:
+                    newPos = 0
+                newCoord = [newPos, self.cellPos[1]]
             elif direction == 'W':
                 newPos = self.cellPos[1] - 1
-                newCoord = [self.cellPos[0], self.cellPos[1] - 1]
+                if newPos < 0:
+                    newPos = 0
+                newCoord = [self.cellPos[0], newPos]
 
-            print("New coord: ", newCoord)
+            #print("New coord: ", newCoord)
 
             # check if the target is there
             if self.canvas[tuple(newCoord)] == 1:
-                print("Target found in {} loops.".format(self.loops))
-                raise SystemExit
+                raise Exception
 
             # place the cell on the canvas
-            if direction == 'N' or direction == 'S':
-                self.setPos([newPos, self.cellPos[1]], 2)
-                #self.cellPos[0] = newPos
-            else:
-                #self.cellPos[1] = newPos
-                self.setPos([self.cellPos[0], newPos], 2)
+            # can raise IndexError
+            self.setPos(newCoord, 2)
+
+            # update the internal cell position
+            #print("update cellpos")
             self.cellPos = newCoord
 
         except IndexError:
-            print("Cell is not moving out of canvas.")
+            #print("Loop n°{}: Not moving cell out of canvas.".format(self.loop))
+            pass
 
+
+    def initialize(self):
+        self.moves = 0
+        # CANVAS
+        self.canvas = np.zeros((self.canvasSize, self.canvasSize), dtype=int)
+        self.targetPos = self.getRandomPos()
+        self.setPos(self.targetPos, 1)
+        self.setInitialCellPos()
+
+    def launchSearch(self):
+        for x in range(0, self.maxMoves):
+            try:
+                self.moveCell()
+                self.moves += 1
+            except Exception:
+                #print("Target found in {} loops.".format(self.moves))
+                self.results.append(self.moves)
+                break
+        self.initialize()
+
+        #print("Target not found :(")
+        #self.results.append(self.maxLoops)
 
     # start script
     def __init__(self):
         # config
         self.canvasSize = 100
-        maxLoops = 1000000
+        self.maxMoves = 10000
+        self.maxLoops = 100
         # end config
 
-        self.loops = 0
+        self.results = []
 
-        # CANVAS
-        self.canvas = np.zeros((self.canvasSize, self.canvasSize), dtype=int)
-        self.targetPos = self.getRandomPos()
-        self.setPos(self.targetPos, 1)
+        self.initialize()
 
-        self.setInitialCellPos()
-        print(self.canvas)
+        for x in range(0, self.maxLoops):
+            self.launchSearch()
 
-        print("Cell pos:", self.cellPos)
+        #print("Cell pos:", self.cellPos)
         # now start the search
-        for x in range(0, maxLoops):
-            self.moveCell()
-            self.loops += 1
-            print(self.canvas)
-        print("Target not found :(")
+
+        meanMoves = int(np.mean(self.results))
+        print("Cell took an average of {} moves to find target in a {}×{} canvas. Tested {} times.".format(meanMoves, self.canvasSize, self.canvasSize, self.maxLoops))
 
 app = Dendryt()
