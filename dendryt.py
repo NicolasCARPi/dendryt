473c506b0c83c4e653754854a705aa3011fce6f4
Nicolas CARPi <nicolas.carpi@curie.fr>
2018-02-25 02:56:26 +0100

    producing pngs

--- dendryt.py
+++ dendryt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
@@ -7,29 +8,45 @@ from random import randint
 from random import choice
 
 class Dendryt():
-    # get a random position on the canvas
     def getRandomPos(self):
+        """
+            Get a random position on the canvas
+        """
         return [randint(0, self.canvasSize - 1), randint(0, self.canvasSize -1)]
 
-    # place the target on canvas
     def setPos(self, pos, value):
+        """
+            Change a value on the canvas
+        """
         self.canvas[tuple(pos)] = value
 
     def setInitialCellPos(self):
+        """
+            Set the starting position of the cell
+        """
         cellPos = self.getRandomPos()
         # if we are on target get a new position, otherwise it's too easy :p
         while True:
             if cellPos != self.targetPos:
-                #self.setPos(cellPos, 2)
-                self.cellPos = cellPos
-                #self.startingPos = cellPos
+                self.cellPos = self.startPos = cellPos
+
+                # make the cell visible on canvas
+                self.setPos(self.cellPos, 255)
                 break
             else:
+                # get a new random position and try again
                 cellPos = self.getRandomPos()
 
+        #with open("results" + os.sep + "loop-" + str(self.currentLoop) + ".txt", "w") as log:
+        #    log.write("Starting at cell position:" + str(self.cellPos))
+        #    log.write("Target is at position:" + str(self.targetPos))
+        #log.close()
 
-    # cell can get N S E or W
+
     def getDirection(self):
+        """
+            Select where to go. Returns 'N', 'S', 'E' or 'W'.
+        """
         # are we still persistant?
         if (randint(0, 100) > self.persistanceIndex):
             # invert persistance mode
@@ -40,35 +57,48 @@ class Dendryt():
             return self.previousDir
 
         directions = ['N', 'E', 'S', 'W']
-        self.previousDir = choice(directions)
-        return self.previousDir
+        return choice(directions)
 
-    # cell position
     def moveCell(self):
+        """
+            Move the cell of one unit in a direction
+        """
 
-        direction = self.getDirection()
+        # save the previous direction
+        direction = self.previousDir = self.getDirection()
 
         try:
             if direction == 'N':
                 newPos = self.cellPos[0] - 1
                 if newPos < 0:
                     newPos = 0
+                if newPos > self.canvasSize - 1:
+                    newPos = self.canvasSize - 1
                 newCoord = [newPos, self.cellPos[1]]
             elif direction == 'E':
                 newPos = self.cellPos[1] + 1
                 if newPos < 0:
                     newPos = 0
+                if newPos > self.canvasSize - 1:
+                    newPos = self.canvasSize - 1
                 newCoord = [self.cellPos[0], newPos]
             elif direction == 'S':
                 newPos = self.cellPos[0] + 1
                 if newPos < 0:
                     newPos = 0
+                if newPos > self.canvasSize - 1:
+                    newPos = self.canvasSize - 1
                 newCoord = [newPos, self.cellPos[1]]
             elif direction == 'W':
                 newPos = self.cellPos[1] - 1
                 if newPos < 0:
                     newPos = 0
+                if newPos > self.canvasSize - 1:
+                    newPos = self.canvasSize - 1
                 newCoord = [self.cellPos[0], newPos]
+            #with open("results" + os.sep + "loop-" + str(self.currentLoop) + ".txt", "a") as log:
+            #    log.write("Moving cell", direction)
+            #    log.close()
 
             #print("New coord:", newCoord)
             # check if the target is there
@@ -77,7 +107,7 @@ class Dendryt():
 
             # place the cell on the canvas
             # can raise IndexError
-            self.setPos(newCoord, 255)
+            self.setPos(newCoord, 128)
 
             # update the internal cell position
             self.cellPos = newCoord
@@ -92,6 +122,9 @@ class Dendryt():
             pass
 
     def getInverseDirection(self, direction):
+        """
+            Go back from where you come
+        """
         if direction == 'N':
             return 'S'
         elif direction == 'E':
@@ -105,11 +138,12 @@ class Dendryt():
         """
             Initialize canvas to zeros. Set the target and initial cell positions.
         """
-        self.persistance = True
+        self.persistance = False
         self.previousDir = 'N'
         self.canvas = np.zeros((self.canvasSize, self.canvasSize), dtype=int)
         # where the target is
         self.targetPos = self.getRandomPos()
+        self.setPos(self.targetPos, 255)
         # create the cell in a random position
         self.setInitialCellPos()
 
@@ -123,8 +157,17 @@ class Dendryt():
             except Exception:
                 #print("Target found in {} moves.".format(move))
                 self.results.append(move)
-                plt.imshow(self.canvas, cmap='binary')
-                plt.show()
+
+                # recolor the start position
+                self.canvas[tuple(self.startPos)] = 200
+
+                # draw image
+                fig = plt.figure()
+                plt.imshow(self.canvas, cmap='Greens')
+                plt.title("Target found in {} moves. Start {}. Target {}".format(move, self.startPos, self.targetPos))
+                #plt.show()
+                fig.savefig("results" + os.sep + "loop-" + str(self.currentLoop) + ".png", dpi=200)
+                plt.close()
                 break
 
     def __init__(self):
@@ -132,19 +175,22 @@ class Dendryt():
             Start the script
         """
         # CONFIG
-        self.canvasSize = 1500
-        self.maxLoops = 100
+        self.canvasSize = 500
+        self.maxLoops = 20
         self.maxMoves = 10000000
-        self.persistanceIndex = 45
+        # between 1 and 100
+        self.persistanceIndex = 69
         # END CONFIG
 
         # where we store the number of moves required to find target
         self.results = []
+        self.currentLoop = 0
 
         self.initialize()
 
         # now do the loops
-        for x in range(0, self.maxLoops):
+        for loop in range(0, self.maxLoops):
+            self.currentLoop = loop
             self.launchSearch()
             self.initialize()
 
